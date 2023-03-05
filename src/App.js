387e86106e99e0806d7d2bce4ec0e73affdec914
AnastasyMeleshko <7634907@mail.ru>
2023-03-05 11:37:55 +0300

    feat: finish list of users app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+// List of users: https://reqres.in/api/users
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+      fetch('https://reqres.in/api/users')
+          .then((res) => res.json())
+          .then(json => {
+          setUsers(json.data);
+      }).catch((err) => {
+          console.warn(err);
+          alert('Error during receiving a list of users');
+      }).finally(() => {
+          setLoading(false);
+      })
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+      setSearchValue(event.target.value);
+  }
+
+  const onClickInvite = (id) => {
+      if (invites.includes(id)) {
+          setInvites((prev) => prev.filter((_id) => _id !== id))
+      } else {
+          setInvites((prev) => [...prev, id]);
+      }
+  }
+
+  const onClickSendInvites = () => {
+      setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+        {
+            success ? (
+                <Success count={invites.length} />
+            ) :  (
+                <Users onChangeSearchValue={onChangeSearchValue}
+                       searchValue={searchValue}
+                       items={users}
+                       isLoading={isLoading}
+                       onClickInvite={onClickInvite}
+                       invites={invites}
+                       onClickSendInvites={onClickSendInvites}
+                />
+            )
+        }
+
     </div>
   );
 }
